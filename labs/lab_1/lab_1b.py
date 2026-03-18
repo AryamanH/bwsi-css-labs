85bb0e08dd17e0dfca3d3cbb88f6a50867d0df12
AryamanH <aryamanhegde2009@gmail.com>
2026-03-18 00:13:45 -0400

    Add value error
    Adding sanitize function to fix the value error possibility

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -36,14 +36,20 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
-
+def requested_sanitized_number():
+        while True:
+            try:
+                number = float(input(prompt))
+                return number
+            except ValueError:
+                print("Enter a valid number.")
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = requested_sanitized_number("Enter the first number: ")
+    num2 = requested_sanitized_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
