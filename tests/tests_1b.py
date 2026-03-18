95c6af6612530dbd793531e35bf38fa11b9f4f15
AryamanH <aryamanhegde2009@gmail.com>
2026-03-18 01:11:35 -0400

    Adding tests

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -11,11 +11,13 @@ def test_addition():
     assert simple_calculator("add", 5, 3) == 8          # Test for positive numbers
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
     assert simple_calculator("add", 0, 0) == 0          # Test for zero addition
+    assert simple_calculator("add", 1, -1) == 0         # Test for negative and positive number
 
 def test_subtraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
     assert simple_calculator("subtract", -2, -2) == 0   # Test for negative numbers
     assert simple_calculator("subtract", 0, 5) == -5    # Test for zero minuend
+    assert simple_calculator("subtract", 6, 5) == 1     # Test for positive numbers
 
 def test_multiplication():
     assert simple_calculator("multiply", 5, 3) == 15    # Test for positive numbers
@@ -26,6 +28,7 @@ def test_division():
     assert simple_calculator("divide", 6, 3) == 2       # Test for positive numbers
     assert simple_calculator("divide", -4, 2) == -2     # Test for negative and positive number
     assert simple_calculator("divide", 5, 2) == 2.5     # Test for division resulting in float
+    assert simple_calculator("divide", -2, 4) == -0.5   # Test for negative and positive + result as float
 
 def test_division_by_zero():
     with pytest.raises(ValueError, match="Cannot divide by zero."):
